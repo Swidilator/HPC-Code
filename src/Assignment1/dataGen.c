b06ed96d6aed86a166fd74d9ce237b0c5d4fbd83
Kyle Weiher <kyleweiher@gmail.com>
2018-03-01 16:50:07 +0200

    Move random seed outside of the function

--- src/Assignment1/dataGen.c
+++ src/Assignment1/dataGen.c
@@ -1,22 +1,22 @@
 #include "dataGen.h"
-#include <time.h>
 #include <stdlib.h>
 
 
 
 double** makeDataset(int dim, int points, int min, int max){
 
-    srand(time(NULL));
+
     
+    //2D array for points
     double** rows = malloc(points * sizeof(double*));
 
     for(int i = 0; i < points; i++){
         double* dataPoint = malloc(dim * sizeof(double));
         for(int j = 0; j < dim; j++){
+            //Make number in the range of what is passed in
             dataPoint[j] = ((max - min) * (double)rand()/(double)RAND_MAX + min);
         }
         rows[i] = dataPoint;
     }
-
     return rows;
 }
